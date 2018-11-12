c4ae3f081f301b5a697a1c188abf4bc521b42671
j-bresson <bresson@ircam.fr>
2018-11-12 15:06:22 +0100

    OM lib

---
+++ omai/omai.lisp
@@ -0,0 +1,38 @@
+;;;===================================================
+;;;
+;;; OMAI - AI tools for OM
+;;;
+;;;===================================================
+;
+;   This program is free software. For information on usage 
+;   and redistribution, see the "LICENSE" file in this distribution.
+;
+;   This program is distributed in the hope that it will be useful,
+;   but WITHOUT ANY WARRANTY; without even the implied warranty of
+;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+;
+;============================================================================
+; Authors: Anders Vinjar, Jean Bresson - 2018
+;============================================================================
+; 
+; LOADER FILE FOR OM6
+;
+;============================================================================
+
+
+(in-package :om)
+
+(om::set-lib-release 1.0 (find-library "omai"))
+
+(compile&load (merge-pathnames "sources/package" *load-pathname*))
+(compile&load (merge-pathnames "sources/descriptors" *load-pathname*))
+
+(om::fill-library 
+ '((nil nil nil nil nil)))
+
+(print 
+	"
+	==============================
+OMAI - AI Tools for OM
+ 	==============================
+	")
---
+++ omai/sources/descriptors.lisp
@@ -0,0 +1,23 @@
+
+;;;===================================================
+;;;
+;;; OMAI - AI tools for OM
+;;;
+;;;===================================================
+;
+;   This program is free software. For information on usage 
+;   and redistribution, see the "LICENSE" file in this distribution.
+;
+;   This program is distributed in the hope that it will be useful,
+;   but WITHOUT ANY WARRANTY; without even the implied warranty of
+;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+;
+;=====================================================
+; Authors: Anders Vinjar, Jean Bresson - 2018
+;=====================================================
+;
+; Descriptors and misc. MIR-related utilities
+;
+;=====================================================
+
+(in-package :omai)
