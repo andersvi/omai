152731e849ee1fdcabc721f4f12f3180f85f3102
j-bresson <bresson@ircam.fr>
2018-11-22 11:45:10 +0100

    cleanup utils

--- cs-features.lisp
+++ cs-features.lisp
@@ -1,6 +1,31 @@
 (in-package :om)
 
+;;; UTILS
 
+(defun testcs (&optional (n 100))
+  (let ((cs (mki 'chord-seq
+		 :lmidic (loop repeat n collect (* 100 (om-random 60 84)))
+		 :lonset (loop for i from 0 to 1200 by 100 collect i)
+		 :ldur '(100))))
+    cs))
+
+(defun testcs2 (&optional (n 100))
+  (let ((cs (mki 'chord-seq
+		 :lmidic (loop repeat n collect (* 100 (om-random 60 84)))
+		 :lvel  (loop repeat n collect (om-random 10 127))
+		 :lonset '(0 100)
+		 :ldur (loop repeat n collect (om-random 10 500)))))
+    cs))
+
+(defun cs->fvecs (cs)
+  (mat-trans
+   (mapcar #'(lambda (l) (om-scale (flat l) 0.0 1.0))
+	   (list (lmidic cs)
+		 (ldur cs)
+		 (lvel cs)))))
+
+;;;===============
+ 
 ;; scratchpad to check uses of feature extraction
 
 (defmethod extract-feature-vectors ((self chord-seq) &optional (parameter-list '(lmidic ldur lvel)) (normalize t))
